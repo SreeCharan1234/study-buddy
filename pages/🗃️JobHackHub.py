746f5bd314c45f3540040c45239e1c89a88db8ce
Sree <119997965+SreeCharan1234@users.noreply.github.com>
2024-10-12 12:21:27 +0530

    Update 🗃️JobHackHub.py

--- pages/🗃️JobHackHub.py
+++ pages/🗃️JobHackHub.py
@@ -23,8 +23,8 @@ from streamlit_extras.echo_expander import echo_expander
 from bs4 import BeautifulSoup
 global s
 k=0
-os.getenv("AIzaSyBIBVb-0Z0QwaucMGOGy8-j_RM22X-4-lE")
-genai.configure(api_key="AIzaSyBIBVb-0Z0QwaucMGOGy8-j_RM22X-4-lE")
+os.getenv("AIzaSyCoc7UW6p0Xj7At538R3oMYkfxG7bSLuaU")
+genai.configure(api_key="AIzaSyCoc7UW6p0Xj7At538R3oMYkfxG7bSLuaU")
 t=[ "Python", "Java", "C++", "JavaScript", "Ruby", "PHP", "Swift", "Kotlin", 
     "C#", "Go", "R", "TypeScript", "Scala", "Perl", "Objective-C", "Dart", 
     "Rust", "Haskell", "MATLAB", "SQL", "HTML/CSS", "React", "Angular", "Vue.js", 
